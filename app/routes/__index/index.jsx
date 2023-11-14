f96c45e1c7c6031c50dabf8f7f811db63ab942f3
Fede <fedelll@gmail.com>
2023-11-14 21:14:28 +0100

    welcome msg by env var

--- app/routes/__index/index.jsx
+++ app/routes/__index/index.jsx
@@ -1,3 +1,12 @@
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+export const loader = async () => {
+  return json({ welcome: process.env.TEST_DEV });
+};
+
 export default function Index() {
-  return <p>Welcome</p>;
+  const { welcome } = useLoaderData();
+
+  return <p>{welcome || 'Welcome'}</p>;
 }
