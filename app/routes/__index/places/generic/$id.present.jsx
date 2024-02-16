60f8511cd166a23be703084375587263a1d817c3
Fede III <fedelll@gmail.com>
2024-02-16 23:21:43 +0100

    removed audio from generic places

--- app/routes/__index/places/generic/$id.present.jsx
+++ app/routes/__index/places/generic/$id.present.jsx
@@ -1,10 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 
 import { useRemoveMenu } from '~/components/hooks/useRemoveMenu';
 import { getPlace } from '~/services/place.server';
-import random from '~/domain/random';
 
 export const loader = async ({ params }) => {
   const place = await getPlace(params.id);
@@ -13,21 +11,7 @@ export const loader = async ({ params }) => {
     throw new Error('Place not found');
   }
 
-  let audioFiles;
-  const path = await import('path');
-  const fs = await import('fs/promises');
-  const publicFolderPath = path.join(
-    process.cwd(),
-    // `public/audio/settlements/${place.type}`
-    `public/audio/settlements/village`
-  );
-  try {
-    audioFiles = await fs.readdir(publicFolderPath);
-  } catch (error) {
-    audioFiles = [];
-  }
-
-  return json({ place, audioFiles });
+  return json({ place });
 };
 
 export const action = async ({ request }) => {
@@ -35,70 +19,10 @@ export const action = async ({ request }) => {
 };
 
 function GenericPlaceForPlayers() {
-  const { place, audioFiles } = useLoaderData();
+  const { place } = useLoaderData();
   const { name, img } = place;
 
   useRemoveMenu();
-
-  const audioRef = useRef();
-  const [track, setTrack] = useState();
-  const [audioCtx, setAudioCtx] = useState();
-  const [gainNode, setGainNode] = useState(null);
-
-  useEffect(() => {
-    if (audioCtx) {
-      setTrack(audioCtx.createMediaElementSource(audioRef.current));
-      setGainNode(audioCtx.createGain());
-    }
-  }, [audioCtx]);
-
-  useEffect(() => {
-    if (track) {
-      track.connect(gainNode).connect(audioCtx.destination);
-
-      if (audioCtx.state === 'suspended') {
-        audioCtx.resume();
-      }
-
-      audioRef.current.play();
-      audioRef.current.dataset.playing = 'true';
-
-      let state =
-        audioRef.current.getAttribute('aria-checked') === 'true' ? true : false;
-      audioRef.current.setAttribute('aria-checked', state ? 'false' : 'true');
-    }
-  }, [track]);
-
-  function startAudio() {
-    if (audioCtx) {
-      if (audioRef.current.dataset.playing === 'true') {
-        audioRef.current.pause();
-        audioRef.current.dataset.playing = 'false';
-      } else {
-        audioRef.current.play();
-        audioRef.current.dataset.playing = 'true';
-      }
-
-      let state =
-        audioRef.current.getAttribute('aria-checked') === 'true' ? true : false;
-      audioRef.current.setAttribute('aria-checked', state ? 'false' : 'true');
-    } else {
-      const AudioContext = window.AudioContext || window.webkitAudioContext;
-      setAudioCtx(new AudioContext());
-    }
-  }
-
-  const [audioFile, setAudioFile] = useState(null);
-  useEffect(() => {
-    setAudioFile(random.element(audioFiles));
-  }, []);
-
-  const [volume, setVolume] = useState(1);
-  useEffect(() => {
-    if (gainNode) {
-      gainNode.gain.value = volume;
-    }
-  }, [volume]);
 
   return (
     <>
@@ -108,43 +32,13 @@ function GenericPlaceForPlayers() {
           {name?.slice(1)}
         </span>
       </h1>
-      {!!audioFile && (
-        <audio
-          loop
-          ref={audioRef}
-          src={`/audio/settlements/village/${audioFile}`}
-          type="audio/mp3"
-        >
-          Your browser does not support the audio element.
-        </audio>
-      )}
       <div className="places__image-container-float">
         <img
           src={img}
           className="places__image places__image--float"
           width="100%"
           height="100%"
-          onClick={startAudio}
         />
-      </div>
-      <div className="places__volume-controls">
-        <input
-          type="range"
-          id="volume"
-          className="places__volume-slider"
-          min="0"
-          max="2"
-          value={volume}
-          list="gain-vals"
-          step="0.01"
-          data-action="volume"
-          orient="vertical"
-          onChange={e => setVolume(e.target.value)}
-        />
-        <datalist id="gain-vals">
-          <option value="0" label="min" />
-          <option value="2" label="max" />
-        </datalist>
       </div>
     </>
   );
